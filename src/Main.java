98537db06e0104150fa900d909691748df087556
Mariane Padilha Brizolla <maribrizolla@gmail.com>
2023-01-23 19:07:05 -0300

    classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso nivel intermediario");
+        curso1.setCargaHoraria(15);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Curso nivel iniciante");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Para os participantes do Bootcamp");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
